1870e4d5705187170be8465e9787463591d8f83e
Miroslav Suba <fawkes@azet.sk>
2021-03-06 00:37:57 +0100

    New feature: Player Map

---
+++ Standalone-Packages/G1-PlayerMap/playerMap_API.d
@@ -0,0 +1,8 @@
+func int GetPlayerMapInstance () {
+	//--> Here you can define which map will be opened in what case (e. g. in Old Camp open map displaying old camp)
+	
+	
+	//<--
+	
+	return 0;
+};
