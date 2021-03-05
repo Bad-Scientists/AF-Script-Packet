90af139b4761f52415753150eb45389aab173fad
Miroslav Suba <fawkes@azet.sk>
2021-03-06 00:45:47 +0100

    adding hook detecting action key (Ctrl)

--- interface.d
+++ interface.d
@@ -10,3 +10,20 @@ func int _getInterfaceScaling () {
     var oCViewStatusBar hpBar; hpBar = _^(MEM_Game.hpBar);
     return fracf(hpBar.zCView_vsizex, Print_ToVirtual(180, PS_X));
 };
+
+/*
+ *	Detection for Ctrl key
+ */
+var int PC_ActionButtonPressed;
+
+func void _hook_oCAIHuman_PC_ActionMove () {
+	PC_ActionButtonPressed = MEM_ReadInt (ESP + 4);
+};
+
+func void G12_GetActionButton_Init () {
+	const int once = 0;
+	if (!once) {
+		//G1 hook len 13, G2A hook len = 9
+		HookEngine (oCAIHuman__PC_ActionMove, MEMINT_SwitchG1G2 (13, 9), "_hook_oCAIHuman_PC_ActionMove");
+	};
+};
