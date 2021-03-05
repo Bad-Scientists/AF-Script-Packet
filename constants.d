da4fa50d1e88835773fb387ac23da74e72df4d24
Miroslav Suba <fawkes@azet.sk>
2021-03-06 00:41:29 +0100

    splitting constants, code refactoring

---
+++ constants.d
@@ -0,0 +1,20 @@
+//const float zMDL_ANI_BLEND_IN_ZERO   = float_MAX;
+//const float zMDL_ANI_BLEND_OUT_ZERO  =-float_MAX;
+const int zMDL_ANIEVENT_MAXSTRING    = 4;
+const int zMAN_VERS                  = 12;
+const int zMDL_MAX_ANIS_PARALLEL     = 6;
+const int zMDL_MAX_MESHLIBS_PARALLEL = 4;
+const int zMDL_VELRING_SIZE          = 8;
+const int MAX_ANIHISTORY             = 16;
+  
+// Animation directions
+/*
+enum zTMdl_AniDir {
+	zMDL_ANIDIR_FORWARD,
+	zMDL_ANIDIR_REVERSE,
+	zMDL_ANIDIR_ENDFASTEST
+};
+*/
+const int AniDir_Forward	= 0;
+const int AniDir_Reverse	= 1;
+const int AniDir_EndFastest	= 2;
