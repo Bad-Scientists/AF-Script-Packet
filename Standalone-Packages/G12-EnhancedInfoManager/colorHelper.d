8d22ded28492192b8ec20368102ab3711e6c0cbb
Miroslav Suba <fawkes@azet.sk>
2021-03-20 18:17:58 +0100

    adding dialog for alpha adjustments

--- Standalone-Packages/G12-EnhancedInfoManager/colorHelper.d
+++ Standalone-Packages/G12-EnhancedInfoManager/colorHelper.d
@@ -6,6 +6,11 @@ FUNC VOID ZS_ColorHelper_Hangaround_Loop () {};
 
 FUNC VOID ZS_ColorHelper_Hangaround_End () {};
 
+var int colorHelperR;
+var int colorHelperG;
+var int colorHelperB;
+var int colorHelperA;
+
 instance colorHelpo1 (Npc_Default)
 {
 	Name[0]		= "Color Helper";
@@ -21,6 +26,12 @@ instance colorHelpo1 (Npc_Default)
 
 	Mdl_SetVisual (self, "HUMANS.MDS");
 	Mdl_SetVisualBody (self, "hum_body_Naked0", 4, 1, "Hum_Head_Pony", 9, 0, -1);
+
+	// reset
+	colorHelperR = 255;
+	colorHelperG = 255;
+	colorHelperB = 255;
+	colorHelperA = 255;
 
 	start_aistate = ZS_ColorHelper_Hangaround;
 };
@@ -43,9 +54,51 @@ instance ColorHelper_Important (C_Info) {
 	permanent	= TRUE;
 };
 */
-var int colorHelperR;
-var int colorHelperG;
-var int colorHelperB;
+
+func string DIA_ColorHelper_BuildHexColor (var int r, var int g, var int b, var int a) {
+	var string _r; _r = dec2hex (r);
+	var string _g; _g = dec2hex (g);
+	var string _b; _b = dec2hex (b);
+	var string _a; _a = dec2hex (a);
+	
+	_r = STR_TrimL (_r, "0");
+	_g = STR_TrimL (_g, "0");
+	_b = STR_TrimL (_b, "0");
+	_a = STR_TrimL (_a, "0");
+
+	if (STR_Len(_r) == 0) { _r = "00"; };
+	if (STR_Len(_g) == 0) { _g = "00"; };
+	if (STR_Len(_b) == 0) { _b = "00"; };
+	if (STR_Len(_a) == 0) { _a = "00"; };
+	
+	if (STR_Len(_r) == 1) { _r = ConcatStrings ("0", _r); };
+	if (STR_Len(_g) == 1) { _g = ConcatStrings ("0", _g); };
+	if (STR_Len(_b) == 1) { _b = ConcatStrings ("0", _b); };
+	if (STR_Len(_a) == 1) { _a = ConcatStrings ("0", _a); };
+
+	var string s; s = "";
+
+	s = ConcatStrings (s, _r);
+	s = ConcatStrings (s, _g);
+	s = ConcatStrings (s, _b);
+	s = ConcatStrings (s, _a);
+
+	s = ConcatStrings (s, " hs@");
+
+	s = ConcatStrings (s, _r);
+	s = ConcatStrings (s, _g);
+	s = ConcatStrings (s, _b);
+	s = ConcatStrings (s, _a);
+
+	s = ConcatStrings (s, " ");
+
+	s = ConcatStrings (s, _r);
+	s = ConcatStrings (s, _g);
+	s = ConcatStrings (s, _b);
+	s = ConcatStrings (s, _a);
+
+	return s;
+};
 
 instance DIA_ColorHelper_RED (C_Info) {
 	npc		= colorHelpo1;
@@ -87,39 +140,9 @@ func int DIA_ColorHelper_RED_Condition () {
 	//Spinner ID CookMeat
 	//newDescription = ConcatStrings (newDescription, "h@");
 	newDescription = ConcatStrings (newDescription, "s@red h@");
+
+	newDescription = ConcatStrings (newDescription, DIA_ColorHelper_BuildHexColor (colorHelperR, colorHelperG, colorHelperB, colorHelperA));
 	
-	var string r; r = dec2hex (colorHelperR);
-	var string g; g = dec2hex (colorHelperG);
-	var string b; b = dec2hex (colorHelperB);
-	
-	r = STR_TrimL (r, "0");
-	g = STR_TrimL (g, "0");
-	b = STR_TrimL (b, "0");
-
-	if (STR_Len(r) == 0) { r = "00"; };
-	if (STR_Len(g) == 0) { g = "00"; };
-	if (STR_Len(b) == 0) { b = "00"; };
-	
-	if (STR_Len(r) == 1) { r = ConcatStrings ("0", r); };
-	if (STR_Len(g) == 1) { g = ConcatStrings ("0", g); };
-	if (STR_Len(b) == 1) { b = ConcatStrings ("0", b); };
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
-
-	newDescription = ConcatStrings (newDescription, " hs@");
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
-
-	newDescription = ConcatStrings (newDescription, " ");
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
-
 	newDescription = ConcatStrings (newDescription, " o@ac@:R (");
 
 	newDescription = ConcatStrings (newDescription, IntToString (colorHelperR));
@@ -177,37 +200,7 @@ func int DIA_ColorHelper_GREEN_Condition () {
 	//newDescription = ConcatStrings (newDescription, "h@");
 	newDescription = ConcatStrings (newDescription, "s@green h@");
 	
-	var string r; r = dec2hex (colorHelperR);
-	var string g; g = dec2hex (colorHelperG);
-	var string b; b = dec2hex (colorHelperB);
-	
-	r = STR_TrimL (r, "0");
-	g = STR_TrimL (g, "0");
-	b = STR_TrimL (b, "0");
-
-	if (STR_Len(r) == 0) { r = "00"; };
-	if (STR_Len(g) == 0) { g = "00"; };
-	if (STR_Len(b) == 0) { b = "00"; };
-	
-	if (STR_Len(r) == 1) { r = ConcatStrings ("0", r); };
-	if (STR_Len(g) == 1) { g = ConcatStrings ("0", g); };
-	if (STR_Len(b) == 1) { b = ConcatStrings ("0", b); };
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
-
-	newDescription = ConcatStrings (newDescription, " hs@");
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
-
-	newDescription = ConcatStrings (newDescription, " ");
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
+	newDescription = ConcatStrings (newDescription, DIA_ColorHelper_BuildHexColor (colorHelperR, colorHelperG, colorHelperB, colorHelperA));
 
 	newDescription = ConcatStrings (newDescription, " o@ac@:G (");
 
@@ -266,37 +259,7 @@ func int DIA_ColorHelper_BLUE_Condition () {
 	//newDescription = ConcatStrings (newDescription, "h@");
 	newDescription = ConcatStrings (newDescription, "s@blue h@");
 	
-	var string r; r = dec2hex (colorHelperR);
-	var string g; g = dec2hex (colorHelperG);
-	var string b; b = dec2hex (colorHelperB);
-	
-	r = STR_TrimL (r, "0");
-	g = STR_TrimL (g, "0");
-	b = STR_TrimL (b, "0");
-
-	if (STR_Len(r) == 0) { r = "00"; };
-	if (STR_Len(g) == 0) { g = "00"; };
-	if (STR_Len(b) == 0) { b = "00"; };
-	
-	if (STR_Len(r) == 1) { r = ConcatStrings ("0", r); };
-	if (STR_Len(g) == 1) { g = ConcatStrings ("0", g); };
-	if (STR_Len(b) == 1) { b = ConcatStrings ("0", b); };
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
-
-	newDescription = ConcatStrings (newDescription, " hs@");
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
-
-	newDescription = ConcatStrings (newDescription, " ");
-
-	newDescription = ConcatStrings (newDescription, r);
-	newDescription = ConcatStrings (newDescription, g);
-	newDescription = ConcatStrings (newDescription, b);
+	newDescription = ConcatStrings (newDescription, DIA_ColorHelper_BuildHexColor (colorHelperR, colorHelperG, colorHelperB, colorHelperA));
 
 	newDescription = ConcatStrings (newDescription, " o@ac@:B (");
 
@@ -312,4 +275,63 @@ func int DIA_ColorHelper_BLUE_Condition () {
 };
 
 func void DIA_ColorHelper_BLUE_Info () {
+};
+
+instance DIA_ColorHelper_ALPHA (C_Info) {
+	npc		= colorHelpo1;
+	nr		= 4;
+	condition	= DIA_ColorHelper_ALPHA_Condition;
+	information	= DIA_ColorHelper_ALPHA_Info;
+	important	= FALSE;
+	permanent	= TRUE;
+	description	= "dummy";
+};
+
+func int DIA_ColorHelper_ALPHA_Condition () {
+	//These are in fact Global variables - we can exploit that for this feature - they will retain their value ;-)
+	var string lastSpinnerID;
+
+	//What is current InfoManagerSpinnerID ?
+	if (Hlp_StrCmp (InfoManagerSpinnerID, "alpha")) {
+		//Setup spinner if spinner ID has changed
+		if (!Hlp_StrCmp (InfoManagerSpinnerID, lastSpinnerID)) {
+			//Restore value
+			InfoManagerSpinnerValue = colorHelperA;
+		};
+
+		//Page Up/Down quantity
+		InfoManagerSpinnerPageSize = 5;
+		
+		//Min/Max value (Home/End keys)
+		InfoManagerSpinnerValueMin = 0;
+		InfoManagerSpinnerValueMax = 255;
+
+		//Update 
+		colorHelperA = InfoManagerSpinnerValue;
+	};
+
+	lastSpinnerID = InfoManagerSpinnerID;
+
+	var string newDescription; newDescription = "";
+	
+	//Spinner ID CookMeat
+	//newDescription = ConcatStrings (newDescription, "h@");
+	newDescription = ConcatStrings (newDescription, "s@alpha h@");
+	
+	newDescription = ConcatStrings (newDescription, DIA_ColorHelper_BuildHexColor (colorHelperR, colorHelperG, colorHelperB, colorHelperA));
+
+	newDescription = ConcatStrings (newDescription, " o@ac@:A (");
+
+	newDescription = ConcatStrings (newDescription, IntToString (colorHelperA));
+	newDescription = ConcatStrings (newDescription, "/");
+	newDescription = ConcatStrings (newDescription, IntToString (255));
+	newDescription = ConcatStrings (newDescription, ")~");
+	
+	//Update description
+	DIA_ColorHelper_ALPHA.description = newDescription;
+	
+	return TRUE;
+};
+
+func void DIA_ColorHelper_ALPHA_Info () {
 };
