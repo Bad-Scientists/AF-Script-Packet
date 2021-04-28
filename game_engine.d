c140adfe25bd167a5211c01f0c64eb4e18a387ad
Miroslav Suba <fawkes@azet.sk>
2021-04-28 10:25:56 +0200

    adjusting address format

--- game_engine.d
+++ game_engine.d
@@ -1,5 +1,5 @@
 func void oCGame_TriggerChangeLevel (var string levelName, var string waypoint) {
-	//0063D480  .text     Debug data           ?TriggerChangeLevel@oCGame@@UAEXABVzSTRING@@0@Z
+	//0x0063D480 public: virtual void __thiscall oCGame::TriggerChangeLevel(class zSTRING const &,class zSTRING const &) 
 	const int oCGame__TriggerChangeLevel_G1 = 6542464;
 
 	//0x006C7AF0 public: virtual void __thiscall oCGame::TriggerChangeLevel(class zSTRING const &,class zSTRING const &)
