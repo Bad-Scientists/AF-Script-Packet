70416ff0ea3af7b6bc8a79942b5da9c2828b5330
Miroslav Suba <fawkes@azet.sk>
2021-11-08 09:46:17 +0100

    adding new function oCMobInter_GetScemeName

--- functions_oCMobInter_engine.d
+++ functions_oCMobInter_engine.d
@@ -1,7 +1,7 @@
 func void oCMobInter_SendStateChange (var int mobPtr, var int fromState, var int toState) {
-	//0x0067D8C0 protected: void __thiscall oCMobInter::SendStateChange(int,int) 
+	//0x0067D8C0 protected: void __thiscall oCMobInter::SendStateChange(int,int)
 	const int oCMobInter__SendStateChange_G1 = 6805696;
-	
+
 	//0x0071ED90 public: void __thiscall oCMobInter::SendStateChange(int,int)
 	const int oCMobInter__SendStateChange_G2 = 7466384;
 
@@ -14,4 +14,20 @@ func void oCMobInter_SendStateChange (var int mobPtr, var int fromState, var int
 		CALL__thiscall (_@ (mobPtr), MEMINT_SwitchG1G2 (oCMobInter__SendStateChange_G1, oCMobInter__SendStateChange_G2));
 		call = CALL_End();
 	};
+};
+
+func string oCMobInter_GetScemeName (var int mobPtr) {
+	//0x0067C970 public: virtual class zSTRING __thiscall oCMobInter::GetScemeName(void)
+	const int oCMobInter__GetScemeName_G1 = 6801776;
+
+	//0x0071DBE0 public: virtual class zSTRING __thiscall oCMobInter::GetScemeName(void)
+	const int oCMobInter__GetScemeName_G2 = 7461856;
+
+	if (!Hlp_Is_oCMobInter (mobPtr)) { return ""; };
+
+	CALL_RetValIszString();
+
+	CALL__thiscall (mobPtr, MEMINT_SwitchG1G2 (oCMobInter__GetScemeName_G1, oCMobInter__GetScemeName_G2));
+
+	return CALL_RetValAszstring ();
 };
