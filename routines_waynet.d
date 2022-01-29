7ad672f482017a935c0b81504960fffae2695079
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-01-29 02:48:00 +0100

    Update routines_waynet.d
    adding class definitions for oCRtnEntry and oCRtnManager
    adding new functions oCRtnManager_RemoveEntry, oCRtnManager_RemoveRoutine and oCRtnManager_RemoveAllRoutines

--- routines_waynet.d
+++ routines_waynet.d
@@ -1,3 +1,170 @@
+/*
+ *	Class definitions
+ */
+class oCRtnEntry {
+	var int hour1; //int hour1;
+	var int min1; //int min1;
+	var int hour2; //int hour2;
+	var int min2; //int min2;
+	var int f_script; //int f_script;
+	var string wpname; //zSTRING wpname;
+	var int inst; //int instance;
+	var int poi; //zCVob* poi;
+	var int npc; //oCNpc* npc;
+	var int cutscene; //oCRtnCutscene* cutscene;
+	var int overlay; //int overlay;
+};
+
+instance oCRtnEntry@ (oCRtnEntry);
+
+//As always no idea if this is correct conversion to primitive types :)
+class oCRtnManager {
+/*
+	struct TRtn_WayBoxLimit {
+	public:
+	int begin;
+	zTBBox3D bbox;
+	float value;
+	oCNpc* npc;
+
+	// user API
+	#include "oCRtnManager_TRtn_WayBoxLimit.inl"
+	};
+*/
+
+/*
+	struct TRtn_WayBox {
+	public:
+	zTBBox3D bbox;
+	oCNpc* npc;
+	int found;
+
+	// user API
+	#include "oCRtnManager_TRtn_WayBox.inl"
+	};
+*/
+
+	var int indexStart[3];	//int
+	var int indexEnd[3];	//int
+	var int world;		//zCWorld*
+	var int nextEntryNode;	//zCListSort<oCRtnEntry>*
+
+	//zCListSort<oCRtnEntry> rtnList;
+	var int rtnList_compare;
+	var int rtnList_data;	//oCRtnEntry*
+	var int rtnList_next;
+
+	//zCListSort<TRtn_WayBox> wayboxListX;
+	var int wayboxListX_compare;
+	var int wayboxListX_data;
+	var int wayboxListX_next;
+
+	//zCListSort<TRtn_WayBox> wayboxListY;
+	var int wayboxListY_compare;
+	var int wayboxListY_data;
+	var int wayboxListY_next;
+
+	//zCListSort<TRtn_WayBox> wayboxListZ;
+	var int wayboxListZ_compare;
+	var int wayboxListZ_datal;
+	var int wayboxListZ_next;
+
+	//zCList<oCNpc> activeList;
+	var int activeList_data;
+	var int activeList_next;
+
+	//zCArraySort<TRtn_WayBoxLimit*> wayboxList[3];
+	var int wayboxList0_array;
+	var int wayboxList0_numAlloc;
+	var int wayboxList0_numInArray;
+	var int wayboxList0_compare;
+
+	var int wayboxList1_array;
+	var int wayboxList1_numAlloc;
+	var int wayboxList1_numInArray;
+	var int wayboxList1_compare;
+
+	var int wayboxList2_array;
+	var int wayboxList2_numAlloc;
+	var int wayboxList2_numInArray;
+	var int wayboxList2_compare;
+};
+
+/*
+ *	Global variables
+ */
+
+var oCRtnManager MEM_RtnMan;
+
+/*
+ *	MEM_RtnMan init function
+ */
+//0x008DC220 class oCRtnManager rtnMan
+const int MEMINT_RtnMan_Address_G1 = 9290272;
+
+//0x00AB31C8 class oCRtnManager rtnMan
+const int MEMINT_RtnMan_Address_G2 = 11219400;
+
+func void MEM_RtnMan_Init () {
+	MEM_RtnMan = _^ (MEMINT_SwitchG1G2 (MEMINT_RtnMan_Address_G1, MEMINT_RtnMan_Address_G2));
+};
+
+/*
+ *
+ */
+func void oCRtnManager_RemoveEntry (var int rtnEntryPtr) {
+	//0x006CE040 public: void __thiscall oCRtnManager::RemoveEntry(class oCRtnEntry *)
+	const int oCRtnManager__RemoveEntry_G1 = 7135296;
+
+	//0x00775E70 public: void __thiscall oCRtnManager::RemoveEntry(class oCRtnEntry *)
+	const int oCRtnManager__RemoveEntry_G2 = 7822960;
+
+	const int call = 0;
+	if (CALL_Begin (call)) {
+		var int rtnManPtr; rtnManPtr = MEMINT_SwitchG1G2 (MEMINT_RtnMan_Address_G1, MEMINT_RtnMan_Address_G2);
+		CALL_PtrParam (_@ (rtnEntryPtr));
+		CALL__thiscall (_@ (rtnManPtr), MEMINT_SwitchG1G2 (oCRtnManager__RemoveEntry_G1, oCRtnManager__RemoveEntry_G2));
+		call = CALL_End ();
+	};
+};
+
+/*
+ *
+ */
+func void oCRtnManager_RemoveRoutine (var int npcPtr) {
+	//0x006CE0C0 public: void __thiscall oCRtnManager::RemoveRoutine(class oCNpc *)
+	const int oCRtnManager__RemoveRoutine_G1 = 7135424;
+
+	//0x00775EF0 public: void __thiscall oCRtnManager::RemoveRoutine(class oCNpc *)
+	const int oCRtnManager__RemoveRoutine_G2 = 7823088;
+
+	const int call = 0;
+	if (CALL_Begin (call)) {
+		var int rtnManPtr; rtnManPtr = MEMINT_SwitchG1G2 (MEMINT_RtnMan_Address_G1, MEMINT_RtnMan_Address_G2);
+		CALL_PtrParam (_@ (npcPtr));
+		CALL__thiscall (_@ (rtnManPtr), MEMINT_SwitchG1G2 (oCRtnManager__RemoveRoutine_G1, oCRtnManager__RemoveRoutine_G2));
+		call = CALL_End ();
+	};
+};
+
+func void oCRtnManager_RemoveAllRoutines () {
+	MEM_RtnMan_Init ();
+
+	var zCListSort list;
+
+	var int rtnPtr; rtnPtr = MEM_RtnMan.rtnList_next;
+	while (rtnPtr);
+		list = _^ (rtnPtr);
+
+		if (list.data) {
+			var oCRtnEntry rtn; rtn = _^ (list.data);
+			oCRtnManager_RemoveRoutine (rtn.npc);
+		};
+
+		rtnPtr = MEM_RtnMan.rtnList_next;
+	end;
+};
+
 // sizeof 20h
 class zCWay {
 	var int cost;	//int cost;          // sizeof 04h    offset 04h
@@ -11,6 +178,9 @@ class zCWay {
 
 instance zCWay@ (zCWay);
 
+/*
+ *
+ */
 func int zCWay_GetGoalWaypoint (var int wayPtr, var int wpPtr) {
 	//0x00704EA0 public: class zCWaypoint * __thiscall zCWay::GetGoalWaypoint(class zCWaypoint *)
 	const int zCWay__GetGoalWaypoint_G1 = 7360160;
