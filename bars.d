5afe4fb00151cd8e6c199ba28caa381c0c2da82b
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-02-06 01:11:22 +0100

    Update bars.d
    function Bar_SetAlphaBackAndBar will not update bar or backgeound alpha values if alpha parameter == -1

--- bars.d
+++ bars.d
@@ -1,9 +1,15 @@
 func void Bar_SetAlphaBackAndBar (var int bar, var int alphaBack, var int alphaBar) {
 	if(!Hlp_IsValidHandle(bar)) { return; };
 	var _bar b; b = get(bar);
-	//Another trick von Mud-freak :)
-	View_SetAlpha(b.v0, alphaBack);
-	View_SetAlpha(b.v1, alphaBar);
+
+	//-1 means do not change this one
+	if (alphaBack > -1) {
+		//Another trick von Mud-freak :)
+		View_SetAlpha(b.v0, alphaBack);
+	};
+	if (alphaBar > -1) {
+		View_SetAlpha(b.v1, alphaBar);
+	};
 };
 
 func void Bar_Top (var int bar) {
