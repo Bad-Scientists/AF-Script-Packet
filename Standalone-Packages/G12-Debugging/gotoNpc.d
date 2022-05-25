66cb54650e39b9a9cc5fb3a9b484ad07af37e628
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-05-25 22:46:12 +0200

    improve feature
    Function now allows teleportation to NPC by their name.

--- Standalone-Packages/G12-Debugging/gotoNpc.d
+++ Standalone-Packages/G12-Debugging/gotoNpc.d
@@ -1,10 +1,66 @@
 /*
- *	Simple debugging feature that allows us teleportation to NPC by their global variables.
- *	For example:
- *	  var C_NPC Diego; Diego = Hlp_GetNpc (PC_Thief);
+ *	Simple debugging feature that allows us teleportation to NPC by their global variables / using npc names
+ *	For example define your own variable, or simply use NPCs name:
+ *	  var C_NPC FriendDiego; FriendDiego = Hlp_GetNpc (PC_Thief);
  *	We can use console command:
+ *	  goto npc FriendDiego
  *	  goto npc Diego
  */
+
+func void PC_BeamToNpc (var int npcInstance) {
+	var oCNpc npc;
+	var int pos[3];
+
+	npc = Hlp_GetNpc (npcInstance);
+	if (!Hlp_IsValidNPC (npc)) { return; };
+
+	//Default position - players position
+	var int posPtr;
+	posPtr = zCVob_GetPositionWorld (_@ (hero));
+	MEM_CopyBytes (posPtr, _@ (pos), 12);
+	MEM_Free (posPtr);
+
+	//Get NPC state
+	var int statePtr; statePtr = NPC_GetNPCState (npc);
+	if (statePtr) {
+		var oCNPC_States state; state = _^ (statePtr);
+
+		//Get routine position
+		if (state.hasRoutine) {
+			posPtr = oCRtnManager_GetRoutinePos (npc);
+			MEM_CopyBytes (posPtr, _@ (pos), 12);
+			MEM_Free (posPtr);
+		} else {
+			//Update AI state position
+			if (!state.aiStateDriven) {
+				//Update aiStateDriven - to kick in ai state
+				state.aiStateDriven = 1;
+
+				//Update ai position
+				npc.state_aiStatePosition[0] = npc._zCVob_trafoObjToWorld[3];
+				npc.state_aiStatePosition[1] = npc._zCVob_trafoObjToWorld[7];
+				npc.state_aiStatePosition[2] = npc._zCVob_trafoObjToWorld[11];
+			};
+
+			//Get AI state position
+			MEM_CopyBytes (_@ (npc.state_aiStatePosition), _@ (pos), 12);
+		};
+	};
+
+	var int ptr; ptr = MEM_GetSymbolByIndex (Hlp_GetInstanceID (npc));
+
+	if (ptr) {
+		var zCPar_Symbol symb; symb = _^ (ptr);
+		PrintS (symb.name);
+
+		if (STR_Len (symb.name)) {
+			//Seems like oCNpc::Enable will spawn NPC (?) :yuppi:
+			oCNpc_Enable (npc, _@ (pos));
+			oCNPC_BeamTo (hero, symb.name);
+		};
+	};
+};
+
 func string CC_GotoNpc (var string param) {
 	var int count; count = STR_SplitCount (param, " ");
 	var string objectName; objectName = "";
@@ -16,6 +72,9 @@ func string CC_GotoNpc (var string param) {
 	};
 
 	if (STR_Len (objectName)) {
+		var oCNpc npc;
+
+		//Get vob
 		var int ptr; ptr = MEM_SearchVobByName (objectName);
 
 		if (ptr) {
@@ -23,65 +82,42 @@ func string CC_GotoNpc (var string param) {
 			return "Vob found.";
 		};
 
+		//Get variable name
 		var int symbID; symbID = MEM_GetSymbolIndex (objectName);
 
 		if (symbID > -1) {
-			var oCNpc npc; npc = Hlp_GetNpc (symbID);
+			npc = Hlp_GetNpc (symbID);
 
 			if (Hlp_IsValidNpc (npc)) {
-				ptr = MEM_GetSymbolByIndex (Hlp_GetInstanceID (npc));
+				PC_BeamToNpc (npc);
+				return "Npc found.";
+			};
+		} else {
+			//Loop through list of Npcs and search by name
+			var int listPtr;
 
-				if (ptr) {
-					var zCPar_Symbol symb; symb = _^ (ptr);
+			listPtr = MEM_World_Get_voblist_npcs ();
 
-					PrintS (symb.name);
+			ptr = 0;
 
-					if (STR_Len (symb.name)) {
-						//--> Force NPC spawn
-						var int pos[3];
+			var zCListSort list;
 
-						//Default position - players position
-						var int posPtr;
-						posPtr = zCVob_GetPositionWorld (_@ (hero));
-						MEM_CopyBytes (posPtr, _@ (pos), 12);
-						MEM_Free (posPtr);
-
-						//Get NPC state
-						var int statePtr; statePtr = NPC_GetNPCState (npc);
-						if (statePtr) {
-							var oCNPC_States state; state = _^ (statePtr);
-
-							//Get routine position
-							if (state.hasRoutine) {
-								posPtr = oCRtnManager_GetRoutinePos (npc);
-								MEM_CopyBytes (posPtr, _@ (pos), 12);
-								MEM_Free (posPtr);
-							} else {
-								//Update AI state position
-								if (!state.aiStateDriven) {
-									//Update aiStateDriven - to kick in ai state
-									state.aiStateDriven = 1;
-
-									//Update ai position
-									npc.state_aiStatePosition[0] = npc._zCVob_trafoObjToWorld[3];
-									npc.state_aiStatePosition[1] = npc._zCVob_trafoObjToWorld[7];
-									npc.state_aiStatePosition[2] = npc._zCVob_trafoObjToWorld[11];
-								};
-
-								//Get AI state position
-								MEM_CopyBytes (_@ (npc.state_aiStatePosition), _@ (pos), 12);
+			while (listPtr);
+				list = _^ (listPtr);
+				if (list.data) {
+					if (Hlp_Is_oCNpc (list.data)) {
+						npc = _^ (list.data);
+						if (Hlp_IsValidNPC (npc)) {
+							if (Hlp_StrCmp (STR_Upper (npc.name), objectName)) {
+								PC_BeamToNpc (npc);
+								return "Npc found.";
 							};
 						};
-
-						//Seems like oCNpc::Enable will spawn NPC (?) :yuppi:
-						oCNpc_Enable (npc, _@ (pos));
-						oCNPC_BeamTo (hero, symb.name);
-						//<--
-
-						return "Npc found.";
 					};
 				};
-			};
+
+				listPtr = list.next;
+			end;
 		};
 	};
 
