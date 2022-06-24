9f9e140ad83cccb66c8949a079551f7e7e31ef26
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-06-24 10:23:23 +0200

    fix function oCItem_SetLightingSwell
    Function was working, but it was incorrectly written. This is the way.

--- functions_oCItem.d
+++ functions_oCItem.d
@@ -20,16 +20,20 @@ func void oCItem_AddFlags (var int itemPtr, var int addFlags) {
 	itm.flags = (itm.flags | (addFlags));
 };
 
-func void oCItem_SetLightingSwell (var int itemPtr, var int value) {
+func void oCItem_SetLightingSwell (var int value) {
 	//0x00670DC0 public: static void __cdecl oCItem::SetLightingSwell(int)
 	const int oCItem__SetLightingSwell_G1 = 6753728;
 
 	//0x00711270 public: static void __cdecl oCItem::SetLightingSwell(int)
-	const int oCItem__SetLightingSwell_G2 = 6753728;
+	const int oCItem__SetLightingSwell_G2 = 7410288;
 
-	CALL_IntParam (value);
-	CALL_PtrParam (itemPtr);
-	CALL__cdecl (MEMINT_SwitchG1G2 (oCItem__SetLightingSwell_G1, oCItem__SetLightingSwell_G2));
+	const int call = 0;
+	if (CALL_Begin (call)) {
+		CALL_IntParam (_@ (value));
+		CALL__cdecl (MEMINT_SwitchG1G2 (oCItem__SetLightingSwell_G1, oCItem__SetLightingSwell_G2));
+		call = CALL_End ();
+	};
+};
 };
 
 
