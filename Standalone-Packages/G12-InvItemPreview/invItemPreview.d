404a0b89dd7c9904137b26a5eda9efac602f5f2e
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-07-03 03:06:08 +0200

    Improve performance for Inventory item preview

--- Standalone-Packages/G12-InvItemPreview/invItemPreview.d
+++ Standalone-Packages/G12-InvItemPreview/invItemPreview.d
@@ -42,20 +42,36 @@ func void GetItemPreviewData__InvItemPreview (var int npcInventoryPtr) {
 		PC_ItemPreviewMana = 0;
 
 		if (itemContainer.inventory2_oCItemContainer_contents) {
-			if ((itemContainer.inventory2_oCItemContainer_selectedItem > -1) && List_LengthS (itemContainer.inventory2_oCItemContainer_contents) > 1) {
+			if (itemContainer.inventory2_oCItemContainer_selectedItem > -1) {
+				//if (List_LengthS (itemContainer.inventory2_oCItemContainer_contents) > 1) {
 
-				var int vobPtr; vobPtr = List_GetS (itemContainer.inventory2_oCItemContainer_contents, itemContainer.inventory2_oCItemContainer_selectedItem + 2);
+				//Here we don't need to get length - all we need to know is whether list is empty :)
+				var zCListSort l; l = _^ (itemContainer.inventory2_oCItemContainer_contents);
+				if (l.next) {
+					const int lastVobPtr = 0;
+					const int lastHealth = 0;
+					const int lastMana = 0;
 
-				if (vobPtr) {
-					PC_ItemPreviewHealth = GetItemCountValue (vobPtr, NAME_Bonus_HP);
-					PC_ItemPreviewMana = GetItemCountValue (vobPtr, NAME_Bonus_Mana);
+					var int vobPtr; vobPtr = List_GetS (itemContainer.inventory2_oCItemContainer_contents, itemContainer.inventory2_oCItemContainer_selectedItem + 2);
+
+					if ((vobPtr) && (vobPtr != lastVobPtr)) {
+						PC_ItemPreviewHealth = GetItemCountValue (vobPtr, NAME_Bonus_HP);
+						PC_ItemPreviewMana = GetItemCountValue (vobPtr, NAME_Bonus_Mana);
+
+						lastHealth = PC_ItemPreviewHealth;
+						lastMana = PC_ItemPreviewMana;
+						lastVobPtr = vobPtr;
+					} else {
+						PC_ItemPreviewHealth = lastHealth;
+						PC_ItemPreviewMana = lastMana;
+					};
 				};
 			};
 		};
 	};
 };
 
-func void _hook_oCItemContainer_CheckSelectedItem () {
+func void _hook_oCItemContainer_Draw () {
 	if (oCItemContainer_IsActive (ECX)) {
 		//Update public variables only when Item preview is active
 		if ((PC_ItemPreviewActive) && (PC_ItemPreviewCanUpdate)) {
@@ -83,10 +99,19 @@ func void G12_InvItemPreview_Init () {
 	const int once = 0;
 	if (!once) {
 		//0x00669560 protected: virtual void __thiscall oCItemContainer::CheckSelectedItem(void)
-		const int oCItemContainer__CheckSelectedItem_G1 = 6722912;
+		//const int oCItemContainer__CheckSelectedItem_G1 = 6722912;
 		//0x00709660 protected: virtual void __thiscall oCItemContainer::CheckSelectedItem(void)
-		const int oCItemContainer__CheckSelectedItem_G2 = 7378528;
-		HookEngine (MEMINT_SwitchG1G2 (oCItemContainer__CheckSelectedItem_G1, oCItemContainer__CheckSelectedItem_G2), 5, "_hook_oCItemContainer_CheckSelectedItem");
+		//const int oCItemContainer__CheckSelectedItem_G2 = 7378528;
+
+		//Performance heavy - seems like this function is called constantly (whenever any item is inserted into inventory, with huge inventories this is causing lags)
+		//HookEngine (MEMINT_SwitchG1G2 (oCItemContainer__CheckSelectedItem_G1, oCItemContainer__CheckSelectedItem_G2), 5, "_hook_oCItemContainer_CheckSelectedItem");
+
+		//0x00667660 protected: virtual void __thiscall oCItemContainer::Draw(void)
+		const int oCItemContainer__Draw_G1 = 6714976;
+		//0x007076B0 protected: virtual void __thiscall oCItemContainer::Draw(void)
+		const int oCItemContainer__Draw_G2 = 7370416;
+
+		HookEngine (MEMINT_SwitchG1G2 (oCItemContainer__Draw_G1, oCItemContainer__Draw_G2), 6, "_hook_oCItemContainer_Draw");
 
 		once = 1;
 	};
