e7672c2b41f54bf9ca6e06243287d04f063e04ba
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-07-23 23:52:33 +0200

    add new console command

--- Standalone-Packages/G12-Debugging/printPos.d
+++ Standalone-Packages/G12-Debugging/printPos.d
@@ -3,7 +3,7 @@
  *	 - will print to zSpy position of focused object.
  */
 
- func string CC_PrintPos (var string param) {
+func string CC_PrintTrafoWrapper (var string param, var int printTrafo) {
 	param = STR_Trim (param, " ");
 
 	var oCNpc her; her = Hlp_GetNpc (hero);
@@ -19,30 +19,64 @@
 	var string msg;
 
 	if (her.focus_vob) {
-		var int pos[3];
+		var int oldErrorLevel; oldErrorLevel = zERROR_GetFilterLevel ();
+		zERROR_SetFilterLevel (1);
 
-		if (zCVob_GetPositionWorldToPos (her.focus_vob, _@ (pos))) {
-			var int oldErrorLevel; oldErrorLevel = zERROR_GetFilterLevel ();
-			zERROR_SetFilterLevel (1);
+		//Print Trafo
+		if (printTrafo) {
+			msg = "const string descRot";
 
-			msg = "const float pos";
 			if (STR_Len (objectName)) {
 				msg = ConcatStrings (msg, "_");
 				msg = ConcatStrings (msg, objectName);
 			};
 
-			msg = ConcatStrings (msg, "[3] = {");
-			msg = ConcatStrings (msg, toStringF (pos[0]));
-			msg = ConcatStrings (msg, ", ");
-			msg = ConcatStrings (msg, toStringF (pos[1]));
-			msg = ConcatStrings (msg, ", ");
-			msg = ConcatStrings (msg, toStringF (pos[2]));
-			msg = ConcatStrings (msg, "};");
+			msg = ConcatStrings (msg, " = ");
+			msg = ConcatStrings (msg, BtoC(34));
+			msg = ConcatStrings (msg, Vob_GetDescriptionRot (her.focus_vob));
+			msg = ConcatStrings (msg, BtoC(34));
+			msg = ConcatStrings (msg, ";");
 
 			MEM_Info (msg);
 
-			zERROR_SetFilterLevel (oldErrorLevel);
+			msg = "const string descPos";
+
+			if (STR_Len (objectName)) {
+				msg = ConcatStrings (msg, "_");
+				msg = ConcatStrings (msg, objectName);
+			};
+
+			msg = ConcatStrings (msg, " = ");
+			msg = ConcatStrings (msg, BtoC(34));
+			msg = ConcatStrings (msg, Vob_GetDescriptionPos (her.focus_vob));
+			msg = ConcatStrings (msg, BtoC(34));
+			msg = ConcatStrings (msg, ";");
+
+			MEM_Info (msg);
+		} else
+		//Print Pos
+		{
+			var int pos[3];
+			if (zCVob_GetPositionWorldToPos (her.focus_vob, _@ (pos))) {
+				msg = "const float pos";
+				if (STR_Len (objectName)) {
+					msg = ConcatStrings (msg, "_");
+					msg = ConcatStrings (msg, objectName);
+				};
+
+				msg = ConcatStrings (msg, "[3] = {");
+				msg = ConcatStrings (msg, toStringF (pos[0]));
+				msg = ConcatStrings (msg, ", ");
+				msg = ConcatStrings (msg, toStringF (pos[1]));
+				msg = ConcatStrings (msg, ", ");
+				msg = ConcatStrings (msg, toStringF (pos[2]));
+				msg = ConcatStrings (msg, "};");
+
+				MEM_Info (msg);
+			};
 		};
+
+		zERROR_SetFilterLevel (oldErrorLevel);
 	} else {
 		msg = "Nothing in focus.";
 	};
@@ -50,6 +84,15 @@
 	return msg;
 };
 
+func string CC_PrintPos (var string param) {
+	return CC_PrintTrafoWrapper (param, 0);
+};
+
+func string CC_PrintTrafo (var string param) {
+	return CC_PrintTrafoWrapper (param, 1);
+};
+
 func void CC_PrintPos_Init () {
 	CC_Register (CC_PrintPos, "print pos", "Will print to zSpy position of focused object.");
+	CC_Register (CC_PrintTrafo, "print trafo", "Will print to zSpy position & rotation of focused object.");
 };
