d30d6e1219445236a10fb508ac3ce401bc8c4bc7
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-10-26 17:21:55 +0200

    add moon phases feature

---
+++ Standalone-Packages/G12-MoonPhases/moonPhases_API.d
@@ -0,0 +1,15 @@
+/*
+
+Copy this function outside of the script packet - define your own rules and textures :)
+
+func void Moon_UpdateTexture () {
+	if (moonPhase == 1) { Moon_SetTexture ("Moon_Phase1_WaxingCrescent.tga"); };
+	if (moonPhase == 2) { Moon_SetTexture ("Moon_Phase2_FirstQuarter.tga"); };
+	if (moonPhase == 3) { Moon_SetTexture ("Moon_Phase3_WaxingGibous.tga"); };
+	if (moonPhase == 4) { Moon_SetTexture ("Moon_Phase4_FullMoon.tga"); };
+	if (moonPhase == 5) { Moon_SetTexture ("Moon_Phase5_WaningGibous.tga"); };
+	if (moonPhase == 6) { Moon_SetTexture ("Moon_Phase6_LastQuarter.tga"); };
+	if (moonPhase == 7) { Moon_SetTexture ("Moon_Phase7_WaningCrescent.tga"); };
+	if (moonPhase == 8) { Moon_SetTexture ("Moon_Phase8_NewMoon.tga"); };
+};
+*/
