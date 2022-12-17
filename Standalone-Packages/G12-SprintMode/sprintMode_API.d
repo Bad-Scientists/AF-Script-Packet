92d6f62572d8909e5251778922fdc5fabba37e51
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-12-17 11:37:48 +0100

    remove API file dependency

--- Standalone-Packages/G12-SprintMode/sprintMode_API.d
+++ Standalone-Packages/G12-SprintMode/sprintMode_API.d
@@ -1,30 +1,32 @@
-const int PC_SprintModeBar_PPosX		= -1;		//If not defined script will put bar right underneath health bar
-const int PC_SprintModeBar_PPosY		= -1;		//If not defined script will put bar right underneath health bar
+/*
+//Copy these constants outside of the script packet - define your own values.
+//If script-packet is updated in the future - your definition will be unaffected.
+
+const int PC_SPRINTMODEBAR_PPOSX = -1;		//If not defined script will put bar right underneath health bar
+const int PC_SPRINTMODEBAR_PPOSY = -1;		//If not defined script will put bar right underneath health bar
 
 //Virtual positions have higher prio than pixel positions.
-const int PC_SprintModeBar_VPosX		= -1;		//if not defined (-1) then PC_SprintModeBar_PPosX will be taken into consideration. Virtual coordinates
-const int PC_SprintModeBar_VPosY		= -1;		//if not defined (-1) then PC_SprintModeBar_PPosY will be taken into consideration. Virtual coordinates
+const int PC_SPRINTMODEBAR_VPOSX = -1;		//if not defined (-1) then PC_SprintModeBar_PPosX will be taken into consideration. Virtual coordinates
+const int PC_SPRINTMODEBAR_VPOSY = -1;		//if not defined (-1) then PC_SprintModeBar_PPosY will be taken into consideration. Virtual coordinates
 
 
-const int BAR_TEX_SPRINTMODE_STAMINA		= 0;		//Bar texture for stamina
-const int BAR_TEX_SPRINTMODE_TIMEDOVERLAY	= 1;		//Bar texture for timed overlays (potions)
+const int BAR_TEX_SPRINTMODE_STAMINA = 0;	//Bar texture for stamina
+const int BAR_TEX_SPRINTMODE_TIMEDOVERLAY = 1;	//Bar texture for timed overlays (potions)
 
-const int BAR_TEX_SPRINTMODE_MAX		= 2;
-
-const string BAR_TEX_SPRINTMODE [BAR_TEX_SPRINTMODE_MAX] = {
-	//"Bar_SprintMode_Stamina.tga",
-	//"Bar_SprintMode_TimedOverlay.tga",
-	"Bar_Misc.tga",						//Default Gothic bar texture (yellow)
-	"Bar_SprintMode_TimedOverlay.tga"			//This is my custom texture - you might have to adjust this one !!
-};
+const string PC_SPRINTMODEBAR_TEX = "BAR_MISC.TGA";
+const string PC_SPRINTMODEBAR_TIMED_TEX = "BAR_SPRINTMODE_TIMEDOVERLAY.TGA";
 
 //Default spint mode overlay
-const string PC_SprintModeOverlayName = "HUMANS_SPRINT.MDS";	//HUMANS_SPRINT.MDS
+const string PC_SPRINTMODEOVERLAYNAME = "HUMANS_SPRINT.MDS";	//HUMANS_SPRINT.MDS
 
 //Don't activate sprint mode if already in sprint mode - or if drunk :)
-const int PC_SPRINTMODE_IGNOREWITHOVERLAYS_MAX = 2;
+const int PC_SPRINTMODE_IGNOREWITHOVERLAYS_COUNT = 2;
 
-const string PC_SPRINTMODE_IGNOREWITHOVERLAYS [PC_SPRINTMODE_IGNOREWITHOVERLAYS_MAX] = {
-	"HUMANS_SPRINT.MDS",
-	"HUMANS_DRUNKEN.MDS"
-};
+const string PC_SPRINTMODE_IGNOREWITHOVERLAYS0 = "HUMANS_SPRINT.MDS";
+const string PC_SPRINTMODE_IGNOREWITHOVERLAYS1 = "HUMANS_DRUNKEN.MDS";
+
+//When removing duplicate timed overlays script can either sum up all timers or get max value
+//0 get max value
+//1 sum up all timers
+const int PC_SPRINTMODEBAR_TIMEDOVERLAY_STACKING = 0;
+*/
