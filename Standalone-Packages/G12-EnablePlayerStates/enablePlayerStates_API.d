d21f5af41c0e21187d4b9ceccf31324247689557
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-12-17 03:38:03 +0100

    remove API file dependency
    All player states have to be defined in function C_CanPlayerUseAIState.

--- Standalone-Packages/G12-EnablePlayerStates/enablePlayerStates_API.d
+++ Standalone-Packages/G12-EnablePlayerStates/enablePlayerStates_API.d
@@ -1,12 +1,28 @@
 /*
- *	Here you can specify what states are allowed for player
- */
-func int CanPlayerUseAIState (var string AIStateName) {
-	if (Hlp_StrCmp (AIStateName, "ZS_WHIRLWIND"))
+//Copy this function outside of the script packet - define your own states.
+//If script-packet is updated in the future - your definition will be unaffected.
+
+func int C_CanPlayerUseAIState (var string AIStateName) {
+	//Default engine ZS states [do not change these]
+	if (Hlp_StrCmp (AIStateName, "ZS_ASSESSMAGIC"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_ASSESSSTOPMAGIC"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_MAGICFREEZE"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_WHIRLWIND"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_SHORTZAPPED"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_ZAPPED"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_PYRO"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_MAGICSLEEP"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_MAGICBURN"))
+
+	// Additional - add as many as you need ...
 	|| (Hlp_StrCmp (AIStateName, "ZS_INVMANAGEMENT"))
-	|| (Hlp_StrCmp (AIStateName, "ZS_PICKPOCKETING")) {
+	|| (Hlp_StrCmp (AIStateName, "ZS_PICKPOCKETING"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_TALK"))
+	|| (Hlp_StrCmp (AIStateName, "ZS_PLAYERTALK"))
+	{
 		return TRUE;
 	};
 
 	return FALSE;
 };
+*/
