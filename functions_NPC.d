73576a97a576d4c870684f9e0cdacbca5ca22fcd
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-12-21 09:12:52 +0100

    move function to G2 emulation file

--- functions_NPC.d
+++ functions_NPC.d
@@ -226,31 +226,6 @@ func int NPC_IsInActiveVobList (var int slfInstance) {
 };
 
 /*
- *	Function checks if Active spell is scroll (all scrolls should have ITEM_MULTI flag)
- *	G1 does not have this function.
- */
-func int NPC_GetActiveSpellIsScroll_G1 (var int slfInstance) {
-	var oCNPC slf; slf = Hlp_GetNPC (slfInstance);
-
-	if (!Hlp_IsValidNPC (slf)) { return FALSE; };
-
-	if (slf.fMode != FMODE_MAGIC) { return FALSE; };
-
-	if (!slf.mag_book) { return FALSE; };
-
-	var oCMag_Book magBook;
-	magBook = _^ (slf.mag_book);
-
-	if (!magBook.spellitems_array) { return FALSE; };
-
-	var int selectedSpell; selectedSpell = MEM_ReadIntArray (magBook.spellitems_array, magBook.spellnr);
-
-	var oCItem spell; spell = _^ (selectedSpell);
-
-	return (spell.flags & ITEM_MULTI);
-};
-
-/*
  *	Function loops through activeOverlays_array and checks if one of them is testOverlay
  * 		usage:	if (NPC_HasOverlay (hero, "HUMANS_DRUNKEN.MDS")) { ...
  */
