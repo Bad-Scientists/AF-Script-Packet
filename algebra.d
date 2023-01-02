8237ddd373ec9bb9784d3c18321178f6363e9a2a
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-01-03 00:54:40 +0100

    add function zMAT4_Mul_zVEC3

--- algebra.d
+++ algebra.d
@@ -47,3 +47,27 @@ func int zVEC3_LengthApprox (var int vecPtr) {
 	return +retVal;
 };
 
+/*
+ *	zMAT4_Mul_zVEC3
+ */
+func void zMAT4_Mul_zVEC3 (var int ptrzMAT4, var int ptrzVEC3, var int targetPtr) {
+	//0x00472D40 class zVEC3 __cdecl operator*(class zMAT4 const &,class zVEC3 const &)
+	const int zMAT4_Mul_zVEC3_G1 = 4664640;
+
+	//0x00474370 class zVEC3 __cdecl operator*(class zMAT4 const &,class zVEC3 const &)
+	const int zMAT4_Mul_zVEC3_G2 = 4670320;
+
+	if ((!ptrzMAT4) || (!ptrzVEC3) || (!targetPtr)) { return; };
+
+	//CALL_RetValIsStruct only supported in disposable calls
+	CALL_RetValIsStruct (12);
+
+	CALL_PtrParam (ptrzVEC3);
+	CALL_PtrParam (ptrzMAT4);
+	CALL__cdecl (MEMINT_SwitchG1G2 (zMAT4_Mul_zVEC3_G1, zMAT4_Mul_zVEC3_G2));
+
+	var int posPtr; posPtr = CALL_RetValAsPtr ();
+	MEM_CopyBytes (posPtr, targetPtr, 12);
+	MEM_Free (posPtr);
+};
+
