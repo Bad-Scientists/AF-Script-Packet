85e674d667ff7fdc4f66707e8748e58df10c94ab
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-02-04 18:34:11 +0100

    replace PC_BeamToNpc with NPC_TeleportToNpc

--- Standalone-Packages/G12-Debugging/gotoNpc.d
+++ Standalone-Packages/G12-Debugging/gotoNpc.d
@@ -7,59 +7,6 @@
  *	  goto npc Diego
  */
 
-func void PC_BeamToNpc (var int npcInstance) {
-	var oCNpc npc;
-	var int pos[3];
-
-	npc = Hlp_GetNpc (npcInstance);
-	if (!Hlp_IsValidNPC (npc)) { return; };
-
-	//Default position - players position
-	if (zCVob_GetPositionWorldToPos (_@ (hero), _@ (pos))) {
-		//...
-	};
-
-	//Get NPC state
-	var int statePtr; statePtr = NPC_GetNPCState (npc);
-	if (statePtr) {
-		var oCNPC_States state; state = _^ (statePtr);
-
-		//Get routine position
-		if (state.hasRoutine) {
-			var int posPtr; posPtr = oCRtnManager_GetRoutinePos (npc);
-			MEM_CopyBytes (posPtr, _@ (pos), 12);
-			MEM_Free (posPtr);
-		} else {
-			//Update AI state position
-			if (!state.aiStateDriven) {
-				//Update aiStateDriven - to kick in ai state
-				state.aiStateDriven = 1;
-
-				//Update ai position
-				npc.state_aiStatePosition[0] = npc._zCVob_trafoObjToWorld[3];
-				npc.state_aiStatePosition[1] = npc._zCVob_trafoObjToWorld[7];
-				npc.state_aiStatePosition[2] = npc._zCVob_trafoObjToWorld[11];
-			};
-
-			//Get AI state position
-			MEM_CopyBytes (_@ (npc.state_aiStatePosition), _@ (pos), 12);
-		};
-	};
-
-	var int ptr; ptr = MEM_GetSymbolByIndex (Hlp_GetInstanceID (npc));
-
-	if (ptr) {
-		var zCPar_Symbol symb; symb = _^ (ptr);
-		PrintS (symb.name);
-
-		if (STR_Len (symb.name)) {
-			//Seems like oCNpc::Enable will spawn NPC (?) :yuppi:
-			oCNpc_Enable (npc, _@ (pos));
-			oCNPC_BeamTo (hero, symb.name);
-		};
-	};
-};
-
 func string CC_GotoNpc (var string param) {
 	param = STR_Trim (param, " ");
 	param = STR_Upper (param);
@@ -91,7 +38,8 @@ func string CC_GotoNpc (var string param) {
 			npc = Hlp_GetNpc (symbID);
 
 			if (Hlp_IsValidNpc (npc)) {
-				PC_BeamToNpc (npc);
+				PrintS (GetSymbolName (Hlp_GetInstanceID (npc)));
+				NPC_TeleportToNpc (hero, npc);
 				return "Npc found.";
 			};
 		} else {
@@ -110,8 +58,9 @@ func string CC_GotoNpc (var string param) {
 					if (Hlp_Is_oCNpc (list.data)) {
 						npc = _^ (list.data);
 						if (Hlp_IsValidNPC (npc)) {
-							if (Hlp_StrCmp (STR_Upper (npc.name), objectName)) {
-								PC_BeamToNpc (npc);
+							if (Hlp_StrCmp (STR_Trim (STR_Upper (npc.name), " "), objectName)) {
+								PrintS (GetSymbolName (Hlp_GetInstanceID (npc)));
+								NPC_TeleportToNpc (hero, npc);
 								return "Npc found.";
 							};
 						};
