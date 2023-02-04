ab247bbf5753d2fef738b15148f689f5782f9b81
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-02-04 18:18:00 +0100

    use function NPC_GetRoutineName

--- Standalone-Packages/G12-Debugging/setRoutine.d
+++ Standalone-Packages/G12-Debugging/setRoutine.d
@@ -14,22 +14,7 @@ func string CC_SetRoutine (var string newRtn) {
 	Npc_ExchangeRoutine (npc, newRtn);
 	AI_ContinueRoutine (npc);
 
-	//0x006C6C10 public: class zSTRING __thiscall oCNpc_States::GetRoutineName(void)
-	const int oCNpc_States__GetRoutineName_G1 = 7105552;
-
-	//0x0076E180 public: class zSTRING __thiscall oCNpc_States::GetRoutineName(void)
-	const int oCNpc_States__GetRoutineName_G2 = 7790976;
-
-	//Here we have inconsistency with class declaration in G1/G2A - different naming, so we have to work with offset instead
-	//oCNpc.state_vfptr	// 0x0470
-	//oCNpc.state_vtbl	// 0x0588
-
-	var int ptr; ptr = _@ (npc);
-	var int offset; offset = MEMINT_SwitchG1G2 (1136, 1416);
-
-	CALL_RetValIszString ();
-	CALL__thiscall (ptr + offset, MEMINT_SwitchG1G2 (oCNpc_States__GetRoutineName_G1, oCNpc_States__GetRoutineName_G2));
-	var string curRtn; curRtn = CALL_RetValAszstring ();
+	var string curRtn; curRtn = NPC_GetRoutineName (npc);
 
 	//Function above returns routine in full format: RTN_ & routineName & _ID
 	//In order to compare routines, we have to convert our newRtn to correct format as well
