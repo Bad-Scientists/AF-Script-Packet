74d00a931f3c3ea636491a3ab2211423bbaabfe5
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-06-10 16:08:38 +0200

    add bring npc console command
    This console command will teleport any Npc to player.

--- Standalone-Packages/G12-Debugging/gotoNpc.d
+++ Standalone-Packages/G12-Debugging/gotoNpc.d
@@ -70,6 +70,63 @@ func string CC_GotoNpc (var string param) {
 	return "Vob/Npc not found.";
 };
 
+func string CC_BringNpc (var string param) {
+	param = STR_Trim (param, " ");
+	param = STR_Upper (param);
+
+	var string objectName;
+	objectName = param;
+
+	if (STR_Len (objectName)) {
+		var oCNpc npc;
+
+		//Get variable name
+		var int symbID; symbID = MEM_GetSymbolIndex (objectName);
+
+		if (symbID > -1) {
+			npc = Hlp_GetNpc (symbID);
+
+			if (Hlp_IsValidNpc (npc)) {
+				PrintS (GetSymbolName (Hlp_GetInstanceID (npc)));
+				NPC_TeleportToNpc (npc, hero);
+				return "Npc found.";
+			};
+		} else {
+			//Loop through list of Npcs and search by name
+			var int listPtr;
+
+			listPtr = MEM_World_Get_voblist_npcs ();
+
+			var zCListSort list;
+
+			while (listPtr);
+				list = _^ (listPtr);
+				if (list.data) {
+					if (Hlp_Is_oCNpc (list.data)) {
+						npc = _^ (list.data);
+						if (Hlp_IsValidNPC (npc)) {
+							if (Hlp_StrCmp (STR_Trim (STR_Upper (npc.name), " "), objectName)) {
+								PrintS (GetSymbolName (Hlp_GetInstanceID (npc)));
+
+								//Enable npc first
+								Wld_EnableNpc (npc);
+
+								NPC_TeleportToNpc (npc, hero);
+								return "Npc found.";
+							};
+						};
+					};
+				};
+
+				listPtr = list.next;
+			end;
+		};
+	};
+
+	return "Vob/Npc not found.";
+};
+
 func void CC_GotoNpc_Init () {
 	CC_Register (CC_GotoNpc, "goto npc", "Will teleport player to specific npc.");
+	CC_Register (CC_BringNpc, "bring npc", "Will teleport npc to player.");
 };
