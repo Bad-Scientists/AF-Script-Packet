a7ac5ff523d7010ddf7e755ba0598670885cc3ac
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-06-10 16:59:50 +0200

    add class TObjectRoutine definition

---
+++ EngineClasses_G1/TObjectRoutine.d
@@ -0,0 +1,8 @@
+// sizeof 24h
+class TObjectRoutine {
+	var string objName; //zSTRING // sizeof 14h offset 00h
+	var int stateNum; //int // sizeof 04h offset 14h
+	var int hour1; //int // sizeof 04h offset 18h
+	var int min1; //int // sizeof 04h offset 1Ch
+	var int type; //int // sizeof 04h offset 20h
+};
