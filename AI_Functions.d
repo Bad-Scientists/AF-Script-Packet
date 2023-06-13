ad2950380a1b1cce990a68f06acf12d6bba994f3
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-06-13 14:38:51 +0200

    tweak AI_TeleportKeepQueue

--- AI_Functions.d
+++ AI_Functions.d
@@ -517,37 +517,9 @@ func void AI_GotoVobPtr (var int slfInstance, var int vobPtr) {
 func void _AI_TeleportKeepQueue (var string vobName) {
 	self = _^ (ECX); //wont be required with future LeGo version (> 2.7.1)
 
-	var int pos[3];
-
-	var zCVob vob;
-	var int vobPtr;
-
-	//Search for vob Name
-	vobPtr = MEM_SearchVobByName (vobName);
-
-	//if vob does not exist - search for waypoint
-	if (!vobPtr) {
-		var int wpPtr; wpPtr = SearchWaypointByName (vobName);
-
-		//if waypoint does not exist - exit
-		if (!wpPtr) { return; };
-
-		var zCWaypoint wp; wp = _^ (wpPtr);
-		copyVector (_@ (wp.pos), _@ (pos));
-	} else {
-		vob = _^ (vobPtr);
-		TrfToPos (_@ (vob.trafoObjToWorld), _@ (pos));
-	};
-
-	vob = Hlp_GetNPC (self);
-
-	//Update hero's position
-	//TODO: teleport to Vob needs to be updated
-	vob.trafoObjToWorld[3] = pos[0];
-	vob.trafoObjToWorld[7] = pos[1];
-	vob.trafoObjToWorld[11] = pos[2];
-
-	Wld_PlayEffect ("SPELLFX_TELEPORT_RING", hero, hero, 0, 0, 0, FALSE);
+	Npc_BeamToKeepQueue (self, vobName);
+	Wld_PlayEffect ("SPELLFX_TELEPORT_RING", self, self, 0, 0, 0, FALSE);
+	Snd_Play ("MFX_TELEPORT_CAST");
 };
 
 func string _AI_GetAniName_T_MAGRUN_2_HEASHOOT () {
@@ -578,7 +550,12 @@ func void AI_TeleportKeepQueue (var int slfInstance, var string vobName) {
 	var C_NPC slf; slf = Hlp_GetNPC (slfInstance);
 	if (!Hlp_IsValidNPC (slf)) { return; };
 
-	AI_PlayAni (slf, _AI_GetAniName_T_MAGRUN_2_HEASHOOT ());
+	var string aniTeleportationStart; aniTeleportationStart = _AI_GetAniName_T_MAGRUN_2_HEASHOOT ();
+
+	if (!Npc_HasAni (slf, aniTeleportationStart)) {
+		AI_PlayAni (slf, aniTeleportationStart);
+	};
+
 	AI_Function_S (slf, _AI_TeleportKeepQueue, vobName);
 	AI_PlayAni (slf, _AI_GetAniName_T_HEASHOOT_2_STAND ());
 };
