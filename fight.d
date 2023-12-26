ed23951edb7ab5cf174ed2210078dd9d90056217
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-12-27 00:33:22 +0100

    add fight functions

--- fight.d
+++ fight.d
@@ -79,3 +79,124 @@ func void FM_SetToRanged (var int slfInstance, var int itemInstanceID) {
 	AI_DrawWeapon_Ext (slf, FMODE_FAR, 0); //Ranged
 };
 
+/*
+ *	Engine functions
+ */
+
+/*
+ *	oCNpc_FightAttackMelee
+ */
+func int oCNpc_FightAttackMelee (var int slfInstance, var int fightMove) {
+	//0x007499D0 private: int __thiscall oCNpc::FightAttackMelee(int)
+	const int oCNpc__FightAttackMelee_G1 = 7641552;
+
+	//0x0067EEC0 private: int __thiscall oCNpc::FightAttackMelee(int)
+	const int oCNpc__FightAttackMelee_G2 = 6811328;
+
+	var oCNpc slf; slf = Hlp_GetNPC (slfInstance);
+	if (!Hlp_IsValidNPC (slf)) { return FALSE; };
+
+	var int slfPtr; slfPtr = _@ (slf);
+
+	var int retVal;
+
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		CALL_PutRetValTo (_@ (retVal));
+		CALL_IntParam (_@ (fightMove));
+		CALL__thiscall (_@ (slfPtr), MEMINT_SwitchG1G2 (oCNpc__FightAttackMelee_G1, oCNpc__FightAttackMelee_G2));
+		call = CALL_End();
+	};
+
+	return + retVal;
+};
+
+/*
+ *	oCNpc_ThinkNextFightAction
+ */
+func int oCNpc_ThinkNextFightAction (var int slfInstance) {
+	//0x00749150 private: int __thiscall oCNpc::ThinkNextFightAction(void)
+	const int oCNpc__ThinkNextFightAction_G1 = 7639376;
+
+	//0x0067E350 private: int __thiscall oCNpc::ThinkNextFightAction(void)
+	const int oCNpc__ThinkNextFightAction_G2 = 6808400;
+
+	var oCNpc slf; slf = Hlp_GetNPC (slfInstance);
+	if (!Hlp_IsValidNPC (slf)) { return FALSE; };
+
+	var int slfPtr; slfPtr = _@ (slf);
+
+	var int retVal;
+
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		CALL_PutRetValTo (_@ (retVal));
+		CALL__thiscall (_@ (slfPtr), MEMINT_SwitchG1G2 (oCNpc__ThinkNextFightAction_G1, oCNpc__ThinkNextFightAction_G2));
+		call = CALL_End();
+	};
+
+	return + retVal;
+};
+
+/*
+ *	oCNpc_FightAttackMagic
+ */
+func int oCNpc_FightAttackMagic (var int slfInstance) {
+	//0x0074A800 private: int __thiscall oCNpc::FightAttackMagic(void)
+	const int oCNpc__FightAttackMagic_G1 = 7645184;
+
+	//0x0067FA60 private: int __thiscall oCNpc::FightAttackMagic(void)
+	const int oCNpc__FightAttackMagic_G2 = 6814304;
+
+	var oCNpc slf; slf = Hlp_GetNPC (slfInstance);
+	if (!Hlp_IsValidNPC (slf)) { return 0; };
+
+	var int retVal;
+	var int slfPtr; slfPtr = _@ (slf);
+
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		CALL_PutRetValTo (_@ (retVal));
+		CALL__thiscall (_@ (slfPtr), MEMINT_SwitchG1G2 (oCNpc__FightAttackMagic_G1, oCNpc__FightAttackMagic_G2));
+		call = CALL_End();
+	};
+
+	return + retVal;
+};
+
+/*
+ *	AI functions
+ */
+
+/*
+ *	AI_Attack_Ext
+ *	 - function allows us to define attack type in parameter subType: EV_ATTACKFORWARD, EV_ATTACKRUN, EV_ATTACKLEFT, EV_ATTACKRIGHT
+ */
+func void AI_Attack_Ext (var int slfInstance, var int subType, var int combo) {
+	var oCNpc slf; slf = Hlp_GetNPC (slfInstance);
+	if (!Hlp_IsValidNPC (slf)) { return; };
+
+	var oCAniCtrl_Human aniCtrl; aniCtrl = _^ (slf.AniCtrl);
+
+	//Default
+	var int aniID; aniID = -1;
+
+	if (subType == EV_ATTACKFORWARD) { aniID = aniCtrl._t_hitf; };
+	if (subType == EV_ATTACKRUN) { aniID = aniCtrl._t_hitfrun; };
+	if (subType == EV_ATTACKLEFT) { aniID = aniCtrl._t_hitl; };
+	if (subType == EV_ATTACKRIGHT) { aniID = aniCtrl._t_hitr; };
+
+	//Default
+	if (aniID == -1) {
+		subType = EV_ATTACKFORWARD;
+		aniID = aniCtrl._t_hitf;
+	};
+
+	var int eMsg; eMsg = oCMsgAttack_Create	(subType, 0, FLOATNULL, aniID, combo);
+
+	//Get Event Manager
+	var int eMgr; eMgr = zCVob_GetEM (_@ (slf));
+
+	//Add new msg to Event Manager
+	zCEventManager_OnMessage (eMgr, eMsg, _@ (slf));
+};
