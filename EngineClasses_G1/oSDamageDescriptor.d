047fb9ab3ec05d957603f80f8b9f2324faf93e52
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2024-01-04 17:02:57 +0100

    remove whitespace

--- EngineClasses_G1/oSDamageDescriptor.d
+++ EngineClasses_G1/oSDamageDescriptor.d
@@ -36,49 +36,49 @@ const int bitfield_oSDamageDescriptor_bIsDead = 4;
 const int bitfield_oSDamageDescriptor_bIsUnconscious = 8;
 
 class oSDamageDescriptor {
-	var int validFields; 		//0 unsigned long dwFieldsValid;
+	var int validFields; //0 unsigned long dwFieldsValid;
 
-	var int attackerVob; 		//4 zCVob* pVobAttacker;
-	var int attackerNPC; 		//8 oCNpc* pNpcAttacker;
-	var int hitVob; 		//12 zCVob* pVobHit;
-	var int hitPfx;			//16 oCVisualFX* pFXHit;
-	var int itemWeapon; 		//20 oCItem* pItemWeapon;
+	var int attackerVob; //4 zCVob* pVobAttacker;
+	var int attackerNpc; //8 oCNpc* pNpcAttacker;
+	var int hitVob; //12 zCVob* pVobHit;
+	var int hitPfx; //16 oCVisualFX* pFXHit;
+	var int itemWeapon; //20 oCItem* pItemWeapon;
 
-	var int spellID;		//24 unsigned long nSpellID;
-	var int spellLevel;		//28 unsigned long nSpellLevel;
+	var int spellID; //24 unsigned long nSpellID;
+	var int spellLevel; //28 unsigned long nSpellLevel;
 
-	var int dmgMode;		//32 unsigned long enuModeDamage;
-	var int weaponMode;		//36 unsigned long enuModeWeapon;
+	var int dmgMode; //32 unsigned long enuModeDamage;
+	var int weaponMode; //36 unsigned long enuModeWeapon;
 
-	var int dmgArray[8];	 	//40 unsigned long aryDamage[oEDamageIndex_MAX];
-	var int dmgTotal;		//72 float fDamageTotal;
-	var int dmgMultiplier;		//76 float fDamageMultiplier;
+	var int dmgArray[8]; //40 unsigned long aryDamage[oEDamageIndex_MAX];
+	var int dmgTotal; //72 float fDamageTotal;
+	var int dmgMultiplier; //76 float fDamageMultiplier;
 
-	var int locationHit[3]; 	//80 zVEC3 vecLocationHit;
-	var int directionFly[3];	//92 zVEC3 vecDirectionFly;
+	var int locationHit[3]; //80 zVEC3 vecLocationHit;
+	var int directionFly[3]; //92 zVEC3 vecDirectionFly;
 
-	var string visualFXStr;		//104 zSTRING strVisualFX;
+	var string visualFXStr; //104 zSTRING strVisualFX;
 
-	var int duration;		//124 float fTimeDuration;
-	var int interval; 		//128 float fTimeInterval;
-	var int dmgPerInterval;		//132 float fDamagePerInterval;
+	var int duration; //124 float fTimeDuration;
+	var int interval; //128 float fTimeInterval;
+	var int dmgPerInterval; //132 float fDamagePerInterval;
 
 	var int bitfield; //136 1 -> Once, 2 -> finished, 4 -> isDead, 8 -> isUnconscious
 	//group {
-	//unsigned long bOnce          : 1;
-	//unsigned long bFinished      : 1;
-	//unsigned long bIsDead        : 1;
+	//unsigned long bOnce : 1;
+	//unsigned long bFinished : 1;
+	//unsigned long bIsDead : 1;
 	//unsigned long bIsUnconscious : 1;
-	//unsigned long lReserved      : 28;
+	//unsigned long lReserved : 28;
 	//};
 
-	var int azimuth;		//140 float fAzimuth;
-	var int elevation;		//144 float fElevation;
-	var int timeCurrent;		//148 float fTimeCurrent;
-	var int dmgReal;		//152 float fDamageReal;
-	var int dmgEffective;		//156 float fDamageEffective;
-	var int dmgArrayEffective[8]; 	//160 unsigned long aryDamageEffective[oEDamageIndex_MAX];
-	var int vobParticleFX;		//192 zCVob* pVobParticleFX;
-	var int particleFX;		//196 zCParticleFX* pParticleFX;
-	var int visualFX;		//200 oCVisualFX* pVisualFX;
+	var int azimuth; //140 float fAzimuth;
+	var int elevation; //144 float fElevation;
+	var int timeCurrent; //148 float fTimeCurrent;
+	var int dmgReal; //152 float fDamageReal;
+	var int dmgEffective; //156 float fDamageEffective;
+	var int dmgArrayEffective[8]; //160 unsigned long aryDamageEffective[oEDamageIndex_MAX];
+	var int vobParticleFX; //192 zCVob* pVobParticleFX;
+	var int particleFX; //196 zCParticleFX* pParticleFX;
+	var int visualFX; //200 oCVisualFX* pVisualFX;
 };
--- EngineClasses_G2/oSDamageDescriptor.d
+++ EngineClasses_G2/oSDamageDescriptor.d
@@ -36,51 +36,51 @@ const int bitfield_oSDamageDescriptor_bIsDead = 4;
 const int bitfield_oSDamageDescriptor_bIsUnconscious = 8;
 
 class oSDamageDescriptor {
-	var int validFields; 		//0 unsigned long dwFieldsValid;
+	var int validFields; //0 unsigned long dwFieldsValid;
 
-	var int attackerVob; 		//4 zCVob* pVobAttacker;
-	var int attackerNpc; 		//8 oCNpc* pNpcAttacker;
-	var int hitVob; 		//12 zCVob* pVobHit;
-	var int hitPfx;			//16 oCVisualFX* pFXHit;
-	var int itemWeapon; 		//20 oCItem* pItemWeapon;
+	var int attackerVob; //4 zCVob* pVobAttacker;
+	var int attackerNpc; //8 oCNpc* pNpcAttacker;
+	var int hitVob; //12 zCVob* pVobHit;
+	var int hitPfx; //16 oCVisualFX* pFXHit;
+	var int itemWeapon; //20 oCItem* pItemWeapon;
 
-	var int spellID;		//24 unsigned long nSpellID;
-	var int spellCat; 		//28 unsigned long nSpellCat;
-	var int spellLevel;		//32 unsigned long nSpellLevel;
+	var int spellID; //24 unsigned long nSpellID;
+	var int spellCat; //28 unsigned long nSpellCat;
+	var int spellLevel; //32 unsigned long nSpellLevel;
 
-	var int dmgMode;		//36 unsigned long enuModeDamage;
-	var int weaponMode;		//40 unsigned long enuModeWeapon;
+	var int dmgMode; //36 unsigned long enuModeDamage;
+	var int weaponMode; //40 unsigned long enuModeWeapon;
 
-	var int dmgArray[8];	 	//44 unsigned long aryDamage[oEDamageIndex_MAX];
-	var int dmgTotal;		//76 float fDamageTotal;
-	var int dmgMultiplier;		//80 float fDamageMultiplier;
+	var int dmgArray[8]; //44 unsigned long aryDamage[oEDamageIndex_MAX];
+	var int dmgTotal; //76 float fDamageTotal;
+	var int dmgMultiplier; //80 float fDamageMultiplier;
 
-	var int locationHit[3]; 	//84 zVEC3 vecLocationHit;
-	var int directionFly[3];	//96 zVEC3 vecDirectionFly;
+	var int locationHit[3]; //84 zVEC3 vecLocationHit;
+	var int directionFly[3]; //96 zVEC3 vecDirectionFly;
 
-	var string visualFXStr;		//108 zSTRING strVisualFX;
+	var string visualFXStr; //108 zSTRING strVisualFX;
 
-	var int duration;		//128 float fTimeDuration;
-	var int interval; 		//132 float fTimeInterval;
-	var int dmgPerInterval;		//136 float fDamagePerInterval;
-	var int dmgDontKill;		//140 int bDamageDontKill;
+	var int duration; //128 float fTimeDuration;
+	var int interval; //132 float fTimeInterval;
+	var int dmgPerInterval; //136 float fDamagePerInterval;
+	var int dmgDontKill; //140 int bDamageDontKill;
 
 	var int bitfield; //144 1 -> Once, 2 -> finished, 4 -> isDead, 8 -> isUnconscious
 	//group {
-	//unsigned long bOnce          : 1;
-	//unsigned long bFinished      : 1;
-	//unsigned long bIsDead        : 1;
+	//unsigned long bOnce : 1;
+	//unsigned long bFinished : 1;
+	//unsigned long bIsDead : 1;
 	//unsigned long bIsUnconscious : 1;
-	//unsigned long lReserved      : 28;
+	//unsigned long lReserved : 28;
 	//};
 
-	var int azimuth;		//float fAzimuth;
-	var int elevation;		//float fElevation;
-	var int timeCurrent;		//float fTimeCurrent;
-	var int dmgReal;		//float fDamageReal;
-	var int dmgEffective;		//float fDamageEffective;
-	var int dmgArrayEffective[8];	//unsigned long aryDamageEffective[oEDamageIndex_MAX];
-	var int vobParticleFX;		//zCVob* pVobParticleFX;
-	var int particleFX;		//zCParticleFX* pParticleFX;
-	var int visualFX;		//oCVisualFX* pVisualFX;
+	var int azimuth; //float fAzimuth;
+	var int elevation; //float fElevation;
+	var int timeCurrent; //float fTimeCurrent;
+	var int dmgReal; //float fDamageReal;
+	var int dmgEffective; //float fDamageEffective;
+	var int dmgArrayEffective[8]; //unsigned long aryDamageEffective[oEDamageIndex_MAX];
+	var int vobParticleFX; //zCVob* pVobParticleFX;
+	var int particleFX; //zCParticleFX* pParticleFX;
+	var int visualFX; //oCVisualFX* pVisualFX;
 };
