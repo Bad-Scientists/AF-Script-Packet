d806f1c505b282bdf600eb93752757d89af713c4
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2024-01-04 16:42:40 +0100

    add new aniCtrl functions

--- anims_engine.d
+++ anims_engine.d
@@ -527,6 +527,45 @@ func void zCModelAniActive_DoCombineAni(var int modelAniActivePtr, var int model
 	};
 };
 
+func int oCAniCtrl_Human_IsInCombo (var int aniCtrlPtr) {
+	//0x00628350 public: int __thiscall oCAniCtrl_Human::IsInCombo(void)
+	const int oCAniCtrl_Human__IsInCombo_G1 = 6456144;
+
+	//0x006B1430 public: int __thiscall oCAniCtrl_Human::IsInCombo(void)
+	const int oCAniCtrl_Human__IsInCombo_G2 = 7017520;
+
+	if (!aniCtrlPtr) { return FALSE; };
+
+	var int retVal;
+
+	const int call = 0;
+	if (CALL_Begin (call)) {
+		CALL_PutRetValTo (_@ (retVal));
+		CALL__thiscall (_@ (aniCtrlPtr), MEMINT_SwitchG1G2 (oCAniCtrl_Human__IsInCombo_G1, oCAniCtrl_Human__IsInCombo_G2));
+		call = CALL_End ();
+	};
+
+	return + retVal;
+};
+
+func void oCAniCtrl_Human_SetLookAtTarget (var int aniCtrlPtr, var int targetVob) {
+	//0x0062CE10 public: void __thiscall oCAniCtrl_Human::SetLookAtTarget(class zCVob *)
+	const int oCAniCtrl_Human__SetLookAtTarget_G1 = 6475280;
+
+	//0x006B6490 public: void __thiscall oCAniCtrl_Human::SetLookAtTarget(class zCVob *)
+	const int oCAniCtrl_Human__SetLookAtTarget_G2 = 7038096;
+
+	//Safety check
+	if (!aniCtrlPtr) { return; };
+
+	const int call = 0;
+	if (CALL_Begin (call)) {
+		CALL_PtrParam (_@ (targetVob));
+		CALL__thiscall (_@ (aniCtrlPtr), MEMINT_SwitchG1G2 (oCAniCtrl_Human__SetLookAtTarget_G1, oCAniCtrl_Human__SetLookAtTarget_G2));
+		call = CALL_End ();
+	};
+};
+
 func void oCAniCtrl_Human_StopLookAtTarget (var int aniCtrlPtr) {
 	//0x0062CF70 public: void __thiscall oCAniCtrl_Human::StopLookAtTarget(void)
 	const int oCAniCtrl_Human__StopLookAtTarget_G1 = 6475632;
