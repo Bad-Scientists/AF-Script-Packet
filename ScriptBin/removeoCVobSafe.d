9aa0c0c648aaba4ce49f064f586916b3dd009d6e
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2024-01-05 01:11:02 +0100

    update to recyclable calls

--- ScriptBin/removeoCVobSafe.d
+++ ScriptBin/removeoCVobSafe.d
@@ -4,7 +4,9 @@
  */
 func void RemoveoCVobSafe (var int vobPtr, var int purgeChildren) {
 	if (!vobPtr) { return; };
-	
+
+	var int worldPtr; worldPtr = _@ (MEM_World);
+
 	if (purgeChildren) {
 		var int vobTreePtr; vobTreePtr = MEM_ReadInt (vobPtr+36);
 
@@ -14,17 +16,25 @@ func void RemoveoCVobSafe (var int vobPtr, var int purgeChildren) {
 		//0x00623960 public: virtual int __thiscall zCWorld::DisposeVobs(class zCTree<class zCVob> *)
 		const int zCWorld__DisposeVobs_G2 = 6437216;
 
-		CALL_PtrParam (vobTreePtr);
-		CALL__thiscall (_@(MEM_World), MEMINT_SwitchG1G2 (zCWorld__DisposeVobs_G1, zCWorld__DisposeVobs_G2));
+		const int call = 0;
+		if (CALL_Begin(call)) {
+			CALL_PtrParam (_@(vobTreePtr));
+			CALL__thiscall (_@(worldPtr), MEMINT_SwitchG1G2 (zCWorld__DisposeVobs_G1, zCWorld__DisposeVobs_G2));
+			call = CALL_End();
+		};
 		return;
 	};
-	
+
 	//0x006D6EF0 public: virtual void __thiscall oCWorld::RemoveVob(class zCVob *)
 	const int oCWorld__RemoveVob_G1 = 7171824;
 
 	//0x007800C0 public: virtual void __thiscall oCWorld::RemoveVob(class zCVob *)
 	const int oCWorld__RemoveVob_G2 = 7864512;
 
-	CALL_PtrParam (vobPtr);
-	CALL__thiscall (_@(MEM_World), MEMINT_SwitchG1G2 (oCWorld__RemoveVob_G1, oCWorld__RemoveVob_G2));
+	const int call2 = 0;
+	if (CALL_Begin(call2)) {
+		CALL_PtrParam (_@(vobPtr));
+		CALL__thiscall (_@(worldPtr), MEMINT_SwitchG1G2 (oCWorld__RemoveVob_G1, oCWorld__RemoveVob_G2));
+		call2 = CALL_End();
+	};
 };
