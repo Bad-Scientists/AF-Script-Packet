8bb40fe65b47338a1df578c893cef35e246769c2
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2024-10-13 11:18:43 +0200

    update trading mul getters and setters
    Moving functions to inventory functions file and making them compatible with G2 NoTR.

--- inventory_oCNpc.d
+++ inventory_oCNpc.d
@@ -19,6 +19,94 @@ NPC_GAME_STEAL
 */
 
 /*
+ *	Credits: functions Trade_SetBuyMultiplier & Trade_SetSellMultiplier originally created by OrcWarriorPL
+ *	https://forum.worldofplayers.de/forum/threads/879891-Skriptpaket-Ikarus-2/page12?p=14836995&viewfull=1#post14836995
+ *
+ *	They were modified to change multipliers for both inventory containers in case of both buying / selling containers.
+ */
+
+//Float
+func void Trade_SetBuyMultiplier (var int mulF) {
+	var oCViewDialogTrade dialogTrade;
+	var oCViewDialogItemContainer itemCont;
+
+	if (MEM_InformationMan.DlgTrade) {
+		dialogTrade = _^ (MEM_InformationMan.DlgTrade);
+
+		//G1
+		if (dialogTrade.dlgInventoryNpc) {
+			itemCont = _^ (dialogTrade.dlgInventoryNpc);
+			itemCont.valueMultiplier = mulF;
+		};
+
+		if (MEMINT_SwitchG1G2(1, 0)) {
+			var int ptr; ptr = _@(dialogTrade.dlgInventoryNpc) + 252;
+			if (ptr) {
+				itemCont = _^ (ptr);
+				itemCont.valueMultiplier = mulF;
+			};
+		};
+	};
+};
+
+//Float
+func void Trade_SetSellMultiplier (var int mulF) {
+	var oCViewDialogTrade dialogTrade;
+	var oCViewDialogItemContainer itemCont;
+
+	if (MEM_InformationMan.DlgTrade) {
+		dialogTrade = _^ (MEM_InformationMan.DlgTrade);
+
+		if (MEMINT_SwitchG1G2(1, 0)) {
+			var int ptr; ptr = _@(dialogTrade.dlgInventoryNpc) + 260;
+			if (ptr) {
+				itemCont = _^ (ptr);
+				itemCont.valueMultiplier = mulF;
+			};
+		};
+
+		if (dialogTrade.dlgInventoryPlayer) {
+			itemCont = _^ (dialogTrade.dlgInventoryPlayer);
+			itemCont.valueMultiplier = mulF;
+		};
+	};
+};
+
+//Float
+func int Trade_GetBuyMultiplier () {
+	var oCViewDialogTrade dialogTrade;
+	var oCViewDialogItemContainer itemCont;
+
+	if (MEM_InformationMan.DlgTrade) {
+		dialogTrade = _^ (MEM_InformationMan.DlgTrade);
+
+		if (dialogTrade.dlgInventoryNpc) {
+			itemCont = _^ (dialogTrade.dlgInventoryNpc);
+			return itemCont.valueMultiplier;
+		};
+	};
+
+	return FLOATNULL;
+};
+
+//Float
+func int Trade_GetSellMultiplier () {
+	var oCViewDialogTrade dialogTrade;
+	var oCViewDialogItemContainer itemCont;
+
+	if (MEM_InformationMan.DlgTrade) {
+		dialogTrade = _^ (MEM_InformationMan.DlgTrade);
+
+		if (dialogTrade.dlgInventoryPlayer) {
+			itemCont = _^ (dialogTrade.dlgInventoryPlayer);
+			return itemCont.valueMultiplier;
+		};
+	};
+
+	return FLOATNULL;
+};
+
+/*
  *	Function returns game_mode
  */
 func int oCNpc_Get_Game_Mode () {
