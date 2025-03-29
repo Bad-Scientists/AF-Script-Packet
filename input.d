b2e3154996149030865a19af8eaa4d664e94e5fb
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2025-03-29 01:38:54 +0100

    add useful input functions

--- input.d
+++ input.d
@@ -1,5 +1,5 @@
 /*
- *
+ *	zCInput_GetFirstBindedLogicalKey
  */
 func int zCInput_GetFirstBindedLogicalKey (var int key) {
 	//0x0086CCA0 class zCInput * zinput
@@ -19,17 +19,87 @@ func int zCInput_GetFirstBindedLogicalKey (var int key) {
 	const int call = 0;
 	if (CALL_Begin(call)) {
 		var int zinputPtr; zinputPtr = MEMINT_SwitchG1G2(zinput_addr_G1, zinput_addr_G2);
-
-		CALL_PutRetValTo (_@ (retVal));
+		CALL_PutRetValTo (_@(retVal));
 		CALL_IntParam(_@(key));
-		CALL__thiscall(zinputPtr, MEMINT_SwitchG1G2 (zCInput__GetFirstBindedLogicalKey_G1, zCInput__GetFirstBindedLogicalKey_G2));
+		CALL__thiscall(zinputPtr, MEMINT_SwitchG1G2(zCInput__GetFirstBindedLogicalKey_G1, zCInput__GetFirstBindedLogicalKey_G2));
 		call = CALL_End();
 	};
 
 	return + (retVal & 255);
 };
 
+/*
+ *	zCInput_IsBinded
+ */
+func int zCInput_IsBinded (var int logicalID, var int key) {
+	//0x0086CCA0 class zCInput * zinput
+	const int zinput_addr_G1 = 8834208;
 
+	//0x008D1650 class zCInput * zinput
+	const int zinput_addr_G2 = 9246288;
+
+	//0x004C3050 public: int __thiscall zCInput::IsBinded(unsigned short,unsigned short)
+	const int zCInput__IsBinded_G1 = 4993104;
+
+	//0x004CC370 public: int __thiscall zCInput::IsBinded(unsigned short,unsigned short)
+	const int zCInput__IsBinded_G2 = 5030768;
+
+	var int retVal;
+
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		var int zinputPtr; zinputPtr = MEMINT_SwitchG1G2(zinput_addr_G1, zinput_addr_G2);
+		CALL_PutRetValTo (_@(retVal));
+		CALL_IntParam(_@(key));
+		CALL_IntParam(_@(logicalID));
+		CALL__thiscall(zinputPtr, MEMINT_SwitchG1G2(zCInput__IsBinded_G1, zCInput__IsBinded_G2));
+		call = CALL_End();
+	};
+
+	return + retVal;
+};
+
+/*
+ *	zCInput_Win32_GetToggled
+ *	 - consecutive calls return FALSE
+ */
+func int zCInput_Win32_GetToggled(var int logicalID) {
+	//0x0086CCA0 class zCInput * zinput
+	const int zinput_addr_G1 = 8834208;
+
+	//0x008D1650 class zCInput * zinput
+	const int zinput_addr_G2 = 9246288;
+
+	//0x004C8630 public: virtual int __thiscall zCInput_Win32::GetToggled(unsigned short)
+	const int zCInput_Win32__GetToggled_G1 = 5015088;
+
+	//0x004D5020 public: virtual int __thiscall zCInput_Win32::GetToggled(unsigned short)
+	const int zCInput_Win32__GetToggled_G2 = 5066784;
+
+	var int retVal;
+
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		var int zinputPtr; zinputPtr = MEMINT_SwitchG1G2(zinput_addr_G1, zinput_addr_G2);
+		CALL_PutRetValTo(_@(retVal));
+		CALL_IntParam(_@(logicalID));
+		CALL__thiscall(zinputPtr, MEMINT_SwitchG1G2(zCInput_Win32__GetToggled_G1, zCInput_Win32__GetToggled_G2));
+		call = CALL_End();
+	};
+
+	return + retVal;
+};
+
+/*
+ *	zCInput_IsBindedToggled
+ */
+func int zCInput_IsBindedToggled(var int logicalID, var int key) {
+	return + (zCInput_IsBinded(logicalID, key) && zCInput_Win32_GetToggled(logicalID));
+};
+
+/*
+ *	zCInputCallback_SetHandleEventTop
+ */
 func void zCInputCallback_SetHandleEventTop(var int inputCallBackPtr) {
 	//0x006FB880 public: void __thiscall zCInputCallback::SetHandleEventTop(void)
 	const int zCInputCallback__SetHandleEventTop_G1 = 7321728;
@@ -46,6 +116,9 @@ func void zCInputCallback_SetHandleEventTop(var int inputCallBackPtr) {
 	};
 };
 
+/*
+ *	zCInputCallback_DoEvents
+ */
 func int zCInputCallback_DoEvents(var int key) {
 	//0x006FB9A0 public: static int __cdecl zCInputCallback::DoEvents(int)
 	const int zCInputCallback__DoEvents_G1 = 7322016;
@@ -61,6 +134,62 @@ func int zCInputCallback_DoEvents(var int key) {
 	};
 };
 
+/*
+ *	zCInputCallback_SetKey **hack
+ */
+func void zCInputCallback_SetKey(var int key) {
+	MEM_WriteInt(ESP + 4, key);
+	EDI = key;
+	MEM_WriteInt(ESP - 40, EDI);
+};
+
+/*
+ *	zCInputCallback_GetEnableHandleEvent
+ */
+func int zCInputCallback_GetEnableHandleEvent(var int inputCallbackPtr) {
+	//0x006FB970 public: int __thiscall zCInputCallback::GetEnableHandleEvent(void)
+	const int zCInputCallback__GetEnableHandleEvent_G1 = 7321968;
+
+	//0x007A5560 public: int __thiscall zCInputCallback::GetEnableHandleEvent(void)
+	const int zCInputCallback__GetEnableHandleEvent_G2 = 8017248;
+
+	if (!inputCallbackPtr) { return 0; };
+
+	var int retVal;
+
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		CALL_PutRetValTo(_@(retVal));
+		CALL__thiscall(_@(inputCallbackPtr), MEMINT_SwitchG1G2(zCInputCallback__GetEnableHandleEvent_G1, zCInputCallback__GetEnableHandleEvent_G2));
+		call = CALL_End();
+	};
+
+	return + retVal;
+};
+
+/*
+ *	zCInputCallback_SetEnableHandleEvent
+ */
+func void zCInputCallback_SetEnableHandleEvent(var int inputCallbackPtr, var int enabled) {
+	//0x006FB8F0 public: void __thiscall zCInputCallback::SetEnableHandleEvent(int)
+	const int zCInputCallback__SetEnableHandleEvent_G1 = 7321840;
+
+	//0x007A54E0 public: void __thiscall zCInputCallback::SetEnableHandleEvent(int)
+	const int zCInputCallback__SetEnableHandleEvent_G2 = 8017120;
+
+	if (!inputCallbackPtr) { return; };
+
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		CALL_IntParam(_@(enabled));
+		CALL__thiscall(_@(inputCallbackPtr), MEMINT_SwitchG1G2(zCInputCallback__SetEnableHandleEvent_G1, zCInputCallback__SetEnableHandleEvent_G2));
+		call = CALL_End();
+	};
+};
+
+/*
+ *	GetCharFromDIK
+ */
 func int GetCharFromDIK(var int key, var int shift, var int alt) {
 	//0x004C7590 char __cdecl GetCharFromDIK(char,char)
 	const int GetCharFromDIK_G1 = 5010832;
@@ -70,15 +199,19 @@ func int GetCharFromDIK(var int key, var int shift, var int alt) {
 
 	var int retVal;
 
-	CALL_PutRetValTo(_@(retVal));
+	const int call = 0;
+	if (CALL_Begin(call)) {
+		CALL_PutRetValTo(_@(retVal));
 
-	if (MEMINT_SwitchG1G2(0, 1)) {
-		CALL_IntParam(alt);
+		if (MEMINT_SwitchG1G2(0, 1)) {
+			CALL_IntParam(_@(alt));
+		};
+
+		CALL_IntParam(_@(shift));
+		CALL_IntParam(_@(key));
+		CALL__cdecl(MEMINT_SwitchG1G2(GetCharFromDIK_G1, GetCharFromDIK_G2));
+		call = CALL_End();
 	};
-
-	CALL_IntParam(shift);
-	CALL_IntParam(key);
-	CALL__cdecl(MEMINT_SwitchG1G2 (GetCharFromDIK_G1, GetCharFromDIK_G2));
 
 	if (retVal == -1) {
 		return + retVal;
@@ -86,4 +219,3 @@ func int GetCharFromDIK(var int key, var int shift, var int alt) {
 
 	return + (retVal & 255);
 };
-
